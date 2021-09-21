f43fd32ff5f3224a36f802dab84d73069cd583c0
XXXTENTACION <88360151+xxxtention-TG@users.noreply.github.com>
2021-09-21 09:59:12 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -60,7 +60,7 @@ async def start(bot, update):
     await bot.send_photo(
         chat_id=update.chat.id,
         photo="https://telegra.ph/file/e79621c352f6bc068820e.jpg",
-        text=Translation.START_TEXT.format(
+        caption=Translation.START_TEXT.format(
                 update.from_user.first_name),
         reply_markup=reply_markup,
         parse_mode="html",
