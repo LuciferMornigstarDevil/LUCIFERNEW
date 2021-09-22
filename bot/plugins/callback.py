7ed3eabcefb243bba4604c6c4e26037aecc0bb02
XXXTENTACION <88360151+xxxtention-TG@users.noreply.github.com>
2021-09-22 11:50:31 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -81,7 +81,7 @@ async def cb_navg(bot, update: CallbackQuery):
 
     if ((index_val + 1 )== max_pages) or ((index_val + 1) == len(results)): # Max Pages
         temp_results.append([
-            InlineKeyboardButton("⏪ Back", callback_data=f"navigate({index_val}|back|{query})")
+            InlineKeyboardButton("«« 𝘽𝘼𝘾𝙆", callback_data=f"navigate({index_val}|back|{query})")
         ])
 
     elif int(index_val) == 0:
@@ -89,13 +89,13 @@ async def cb_navg(bot, update: CallbackQuery):
 
     else:
         temp_results.append([
-            InlineKeyboardButton("⏪ Back", callback_data=f"navigate({index_val}|back|{query})"),
-            InlineKeyboardButton("Next ⏩", callback_data=f"navigate({index_val}|next|{query})")
+            InlineKeyboardButton("«« 𝘽𝘼𝘾𝙆", callback_data=f"navigate({index_val}|back|{query})"),
+            InlineKeyboardButton("𝙉𝙀𝙓𝙏 »»", callback_data=f"navigate({index_val}|next|{query})")
         ])
 
     if not int(index_val) == 0:    
         temp_results.append([
-            InlineKeyboardButton(f"🔰 Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton(f"🗒️ Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} 🗒️", callback_data="ignore")
         ])
     
     if show_invite and int(index_val) !=0 :
