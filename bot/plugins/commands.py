73262de77a6f3ac77ebfeb6138daa5a141a31fcb
LuciferMornigstarDevil <91133525+LuciferMornigstarDevil@users.noreply.github.com>
2021-09-23 23:04:59 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -74,6 +74,18 @@ async def start(bot, update):
         reply_to_message_id=update.message_id
     )
 
+reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await bot.send_photo(
+        chat_id=update.chat.id,
+        photo="https://telegra.ph/file/00f3ec42ec504538e8562.jpg",
+        caption=Translation.START_TEXT.format(
+                update.from_user.first_name),
+        reply_markup=reply_markup,
+        parse_mode="html",
+        reply_to_message_id=update.message_id
+    )
+
 
 @Client.on_message(filters.command(["help"]) & filters.private, group=1)
 async def help(bot, update):
