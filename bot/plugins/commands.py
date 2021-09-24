b9b4e35f0090afc30f307b8b92b7df98038cfe79
LuciferMornigstarDevil <91133525+LuciferMornigstarDevil@users.noreply.github.com>
2021-09-24 13:39:52 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) @AlbertEinsteinTG
+#PEAKYBOTEVA | @MR-JINN-OF-TG
 
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from bot import Translation, LOGGER # pylint: disable=import-error
 from bot.database import Database # pylint: disable=import-error
+import random
+
+PHOTOS = [
+    "https://telegra.ph/file/00f3ec42ec504538e8562.jpg",
+    "https://telegra.ph/file/c8d504199f35cbef9e7d9.jpg",
+    "https://telegra.ph/file/6cde654fda604d8766bfc.jpg",
+    "https://telegra.ph/file/842ec4694de82608b18cb.jpg",
+    "https://telegra.ph/file/2ab65224486fff9036d60.jpg",
+    "https://telegra.ph/file/a1e13e0dee2925389d45d.jpg",
+    "https://telegra.ph/file/912e99f6798a3d2b1df23.jpg",
+    "https://telegra.ph/file/4c9b2db0dae674721311a.jpg",
+    "https://telegra.ph/file/c8d504199f35cbef9e7d9.jpg",
+    "https://telegra.ph/file/ce503a23b17d53e630e8f.jpg",
+    "https://telegra.ph/file/715f3323eff6db3be2c6a.jpg",
+    "https://telegra.ph/file/7060eae48294db4fe794c.jpg",
+    "https://telegra.ph/file/260e45cb876f25b1c68ac.jpg",
+    "https://telegra.ph/file/99303a13e5327748f6c9e.jpg",
+    "https://telegra.ph/file/68eda27d26ade468d8b63.jpg",
+    "https://telegra.ph/file/ae952c29c5b6f734cb243.jpg",
+    "https://telegra.ph/file/9eb707d924df4f939f404.jpg",
+    "https://telegra.ph/file/52174d04e4e8fb6a60c3e.jpg",
+]
 
 db = Database()
 
